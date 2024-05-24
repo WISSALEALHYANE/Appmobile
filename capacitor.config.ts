31aa59f2615d2117ebfb5c2f72d7cc1e2f1af4ce
WISSALEALHYANE <149794690+WISSALEALHYANE@users.noreply.github.com>
2024-05-24 16:48:31 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppPresence',
+  webDir: 'www'
+};
+
+export default config;
